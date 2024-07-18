e69ccfc3d11a467c6e68b45f7289a5b34159057c
Angel Alejandro <angel.daniel.alejandro@outlook.com>
2024-07-18 12:35:38 -0600

    parcial 2

---
+++ src/parcialprogra2/Parcialprogra2.java
@@ -0,0 +1,21 @@
+
+package parcialprogra2;
+
+
+public class Parcialprogra2 {
+
+    
+    public static void main(String[] args) {
+       int valores[][]={{5,7,9},{4,2,1},{1,1,1}};
+       int t=3;
+        for (int i = 0; i < valores.length; i++) {
+            for (int j = 0; j <valores.length; j++) {
+                
+               
+                System.out.print(valores[i][j]+"|");
+            }
+        }
+        
+    }
+    
+}
